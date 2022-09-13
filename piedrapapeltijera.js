eb6d86b61e0e28f3cdc4d6ef74a92bfc23504ccc
jona-11 <jonathangalimberti@hotmail.com>
2022-09-13 17:01:36 -0300

    minificacion de codigo por medio de funciones

--- piedrapapeltijera.js
+++ piedrapapeltijera.js
@@ -8,267 +8,73 @@ let conteoPc = document.getElementById("conteoPc");
 let contadorUsuario = [];
 let contadorPC = [];
 
+function combate (usuario1, pc){
 
+    if (usuario1 == "piedra" && pc == "tijeras" || usuario1 == "tijeras" && pc=="papel" || usuario1 == "papel" && pc=="piedra"){
+        eleccionPc.innerHTML= "tu oponente eligio " + pc ;
+        resultadoFinal.innerHTML= "GANASTE!! 😁";
+        contadorUsuario.push(1);
+    }else if (usuario1 == pc){
+        eleccionPc.innerHTML= "tu oponente eligio " + pc ;
+        resultadoFinal.innerHTML= "empate";
+    }else {
+        eleccionPc.innerHTML= "tu oponente eligio " + pc ;
+        resultadoFinal.innerHTML= "perdiste 😢";
+        contadorPC.push(1);
+
+    }
+    conteoPc.innerHTML = "PC: " + contadorPC.length;
+    conteoUsusario.innerHTML = "Jugador: " + contadorUsuario.length;
+
+                 
+     if (contadorPC.length == 10){
+         alert ("tu oponente ganó por "+ (contadorPC.length-contadorUsuario.length)+ " puntos");
+         location.reload();
+     }
+     if (contadorUsuario.length==10){
+         alert ("ganaste por "+ (contadorUsuario.length- contadorPC.length)+ " puntos");
+         location.reload();
+         
+     }    
+    
+}
+
+
+function computadoraResultado(){
+    let resultado = Math.floor(Math.random() * (3 - 1 + 1)) + 1;
+        if(resultado == 2 ){
+            return "papel"
+        }else if(resultado == 3 ){
+            return "tijeras"
+        }else{
+            return "piedra";
+        }
+ }
 
 function papel(){
             
-            const usuario1 ="papel";
-
-            let resultado = computadoraResultado();
-            function computadoraResultado(){
-                let resultado;
-                resultado = Math.floor(Math.random() * (10 - 1 + 1)) + 1;
-                return resultado;
-                //return sirve para devolver un valor a la funcion//
-            }
-            console.log(computadora());
-            
-            function computadora(){
-            
-                if(resultado % 2 == 0){
-                    return "papel"
-                }
-                else if(resultado % 3 == 0 ){
-                    return "tijeras"
-                }
-                else
-                return "piedra";
-            
-            }
-            
-        
-             if(computadora() === "piedra"){
-        
-               
-                eleccionPc.innerHTML= "tu oponente eligio piedra" ;
-            
-        
-            }if(computadora() === "papel"){
-                eleccionPc.innerHTML= "tu oponente eligio papel" ;
-                
-            }if(computadora() === "tijeras"){
-                eleccionPc.innerHTML= "tu oponente eligio tijeras" ;
-                
-            } 
-        
-            if(usuario1 == "piedra" && computadora() === "tijeras"){
-                resultadoFinal.innerHTML= "GANASTE!!";
-                contadorUsuario.push(1);
-                
-                
-            }
-            if(usuario1 == "tijeras" && computadora() === "papel"){
-                resultadoFinal.innerHTML= "GANASTE!!";
-                contadorUsuario.push(1);
-                
-            }
-             if(usuario1 == "papel" && computadora() === "piedra"){
-                resultadoFinal.innerHTML= "GANASTE!!";
-                contadorUsuario.push(1);
-            }
-            if(usuario1 == "piedra" && computadora() === "papel"){
-                resultadoFinal.innerHTML= "perdiste :(";
-                contadorPC.push(1);
-                
-            }
-             if(usuario1 == "tijeras" && computadora() === "piedra"){
-                 resultadoFinal.innerHTML= "perdiste :(";
-                 contadorPC.push(1);
-                }
-                if(usuario1 == "papel" && computadora() === "tijeras"){
-                    resultadoFinal.innerHTML= "perdiste :(";
-                    contadorPC.push(1);
-                }
-             if(usuario1 == computadora()){
-                resultadoFinal.innerHTML= "empate";
-                
-            }
-            conteoPc.innerHTML = "PC: " + contadorPC.length;
-            conteoUsusario.innerHTML = "Jugador: " + contadorUsuario.length;
-
-            if (contadorPC.length == 10){
-                alert ("tu oponente ganó por "+ (contadorPC.length-contadorUsuario.length)+ " puntos");
-                location.reload();
-             }
-             if (contadorUsuario.length==10){
-                alert ("ganaste por "+ (contadorUsuario.length- contadorPC.length)+ " puntos");
-                location.reload();
-                 
-             }
-        
-            
-                       }
+    const usuario1 ="papel";
+    let pc=computadoraResultado();
+                           
+    combate(usuario1,pc);
+}
 
 function piedra(){
             
-            const usuario1 ="piedra";
-            let resultado = computadoraResultado();
-            function computadoraResultado(){
-                let resultado;
-                resultado = Math.floor(Math.random() * (10 - 1 + 1)) + 1;
-                return resultado;
-                //return sirve para devolver un valor a la funcion//
-            }
-            console.log(computadora());
-            
-            function computadora(){
-            
-                if(resultado % 2 == 0){
-                    return "papel"
-                }
-                else if(resultado % 3 == 0 ){
-                    return "tijeras"
-                }
-                else
-                return "piedra";
-            
-            }
-            
-        
-             if(computadora() === "piedra"){
-        
-               
-                eleccionPc.innerHTML= "tu oponente eligio piedra" ;
-            
-        
-            }if(computadora() === "papel"){
-                eleccionPc.innerHTML= "tu oponente eligio papel" ;
-                
-            }if(computadora() === "tijeras"){
-                eleccionPc.innerHTML= "tu oponente eligio tijeras" ;
-                
-            } 
-        
-            if(usuario1 == "piedra" && computadora() === "tijeras"){
-                resultadoFinal.innerHTML= "GANASTE!!";
-                contadorUsuario.push(1);
-                
-            }
-            if(usuario1 == "tijeras" && computadora() === "papel"){
-                resultadoFinal.innerHTML= "GANASTE!!";
-                contadorUsuario.push(1);
-            }
-             if(usuario1 == "papel" && computadora() === "piedra"){
-                resultadoFinal.innerHTML= "GANASTE!!";
-                contadorUsuario.push(1);
-            }
-            if(usuario1 == "piedra" && computadora() === "papel"){
-                resultadoFinal.innerHTML= "perdiste :(";
-                contadorPC.push(1);
-                
-            }
-             if(usuario1 == "tijeras" && computadora() === "piedra"){
-                 resultadoFinal.innerHTML= "perdiste :(";
-                 contadorPC.push(1);
-                }
-                if(usuario1 == "papel" && computadora() === "tijeras"){
-                    resultadoFinal.innerHTML= "perdiste :(";
-                    contadorPC.push(1);
-                }
-             if(usuario1 == computadora()){
-                resultadoFinal.innerHTML= "empate";
-                
-            }
-            conteoPc.innerHTML = "PC: " + contadorPC.length;
-            conteoUsusario.innerHTML = "Jugador: " + contadorUsuario.length;
-        
- 
-            if (contadorPC.length == 10){
-                alert ("tu oponente ganó por "+ (contadorPC.length-contadorUsuario.length)+ " puntos");
-                location.reload();
-             }
-             if (contadorUsuario.length==10){
-                alert ("ganaste por "+ (contadorUsuario.length- contadorPC.length)+ " puntos");
-                location.reload();
-                 
-             }
-                       }
-
+    const usuario1 ="piedra";
+    let pc=computadoraResultado();
+                           
+    combate(usuario1,pc);
+  }
 function tijeras(){
-            
-            const usuario1 ="tijeras";
-            let resultado = computadoraResultado();
-            function computadoraResultado(){
-                let resultado;
-                resultado = Math.floor(Math.random() * (10 - 1 + 1)) + 1;
-                return resultado;
-                //return sirve para devolver un valor a la funcion//
-            }
-            console.log(computadora());
-            
-            function computadora(){
-            
-                if(resultado % 2 == 0){
-                    return "papel"
-                }
-                else if(resultado % 3 == 0 ){
-                    return "tijeras"
-                }
-                else
-                return "piedra";
-            
-            }
-            
-        
-             if(computadora() === "piedra"){
-        
-               
-                eleccionPc.innerHTML= "tu oponente eligio piedra" ;
-            
-        
-            }if(computadora() === "papel"){
-                eleccionPc.innerHTML= "tu oponente eligio papel" ;
-                
-            }if(computadora() === "tijeras"){
-                eleccionPc.innerHTML= "tu oponente eligio tijeras" ;
-                
-            } 
-        
-            if(usuario1 == "piedra" && computadora() === "tijeras"){
-                resultadoFinal.innerHTML= "GANASTE!!";
-                contadorUsuario.push(1);
-                
-            }
-            if(usuario1 == "tijeras" && computadora() === "papel"){
-                resultadoFinal.innerHTML= "GANASTE!!";
-                contadorUsuario.push(1);
-            }
-             if(usuario1 == "papel" && computadora() === "piedra"){
-                resultadoFinal.innerHTML= "GANASTE!!";
-                contadorUsuario.push(1);
-            }
-            if(usuario1 == "piedra" && computadora() === "papel"){
-                resultadoFinal.innerHTML= "perdiste :(";
-                contadorPC.push(1);
-                
-            }
-             if(usuario1 == "tijeras" && computadora() === "piedra"){
-                 resultadoFinal.innerHTML= "perdiste :(";
-                 contadorPC.push(1);
-                }
-                if(usuario1 == "papel" && computadora() === "tijeras"){
-                    resultadoFinal.innerHTML= "perdiste :(";
-                    contadorPC.push(1);
-                }
-             if(usuario1 == computadora()){
-                resultadoFinal.innerHTML= "empate";
-                
-            }
-            conteoPc.innerHTML = "PC: " + contadorPC.length;
-            conteoUsusario.innerHTML = "Jugador: " + contadorUsuario.length;
-           
-            if (contadorPC.length == 10){
-                alert ("tu oponente ganó por "+ (contadorPC.length-contadorUsuario.length)+ " puntos");
-                location.reload();
-             }
-             if (contadorUsuario.length==10){
-                alert ("ganaste por "+ (contadorUsuario.length- contadorPC.length)+ " puntos");
-                location.reload();
-                 
-             }
-            
-                       }
+   const usuario1 = "tijeras";
+   let pc=computadoraResultado();
+                           
+   combate(usuario1,pc);
+                          
 
+  }
+                    
                      
 
     
